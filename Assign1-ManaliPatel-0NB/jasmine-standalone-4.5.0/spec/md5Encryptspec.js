1cf1748e1600f43b9c4c23d449e26c1887c42c9c
manali1321 <patelmanali216@gmail.com>
2023-02-14 17:20:29 -0500

    conncted with 2 spec; error solve submission done

--- Assign1-ManaliPatel-0NB/jasmine-standalone-4.5.0/spec/md5Encryptspec.js
+++ Assign1-ManaliPatel-0NB/jasmine-standalone-4.5.0/spec/md5Encryptspec.js
@@ -9,35 +9,3 @@ describe('test to pass: md5Encryptsec', function () {
     expect(md5Encrypt('123')).toEqual("202cb962ac59075b964b07152d234b70");
   });
 });
-
-// log in spec
-// log in with right value
-describe('test to pass: Login Spec', function () {
-  it("should use the md5Encrypt function, and return the Boolean true", function () {
-    expect(checkLogin('manali', '123')).toEqual(true);
-  });
-});
-// with wrong value
-describe('test to fail: Login Spec with wrong input', function () {
-  it("should return ‘Invalid Username or Password.’", function () {
-    expect(checkLogin('manaliddddd', '123')).toEqual("Invalid Username or Password.");
-  });
-  it("should return ‘Invalid Username or Password.’", function () {
-    expect(checkLogin('manali', '12345')).toEqual("Invalid Username or Password.");
-  });
-  it("should return ‘Invalid Username or Password.’", function () {
-    expect(checkLogin('nidhi', '789')).toEqual("Invalid Username or Password.");
-  });
-});
-// no username
-describe('test to fail: Login Spec with no username', function () {
-  it("should return ‘No username entered.’", function () {
-    expect(checkLogin('', '123')).toEqual("No username entered.");
-  });
-});
-// no password
-describe('test to fail: Login Spec with no password', function () {
-  it("should return ‘No password entered.’", function () {
-    expect(checkLogin('manali', '')).toEqual("No password entered.");
-  });
-});
